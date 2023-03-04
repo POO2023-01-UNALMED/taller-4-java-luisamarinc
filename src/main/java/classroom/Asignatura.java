ccd22492c4febdfc82825885cddf778bf5e1d698
Luisa <lumarinc@unal.edu.co>
2023-03-04 14:21:42 -0500

    taller finalizado

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -3,20 +3,17 @@ package classroom;
 public class Asignatura {
 
     public String nombre;
-    String n = nombre;
+    public String n = nombre;
     public int codigoInterno;
     public int codigoExterno;
     public Tipo tipo;
 
     public Asignatura() {
         this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
+        
     }
 
-    public Asignatura(int codigoInterno,Tipo tipo) {
-        this("Sin nombre", codigoInterno, 0,tipo);
-    }
-
+  
     public Asignatura(int codigoExterno,Tipo tipo) {
         this("Sin nombre", 0, codigoExterno,tipo);
     }
@@ -38,11 +35,7 @@ public class Asignatura {
         this.nombre = nombre;
         this.tipo = tipo;
     }
-
-    public void cambiarDatos(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
-    }
-
+  
     public void cambiarDatos(int codigoExterno) {
         this.codigoExterno = codigoExterno;
     }
